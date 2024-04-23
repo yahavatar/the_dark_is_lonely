d48ceaed8573484aa6b3fd25f02e199e949cf8e5
BTDMIS <122453016+BTDMIS@users.noreply.github.com>
2024-04-23 15:26:04 -0400

    03:26pm

--- mj_if_engine.js
+++ mj_if_engine.js
@@ -82,71 +82,6 @@ function parse_input(){
         }
     }
 
-
-    /*
-    //Check for exit commands:
-    let room_id = lGet("current_room");
-    let room = JSON.parse(lGet(room_id));
-    let exits = room.exits;
-    console.log(exits.length);
-    for (let i = 0; i < exits.length; i++){
-        for (let j = 0; j < exits[i].cmd.length; j++){
-            if (input == exits[i].cmd[j]){
-                took_action = 1;
-                let dest = exits[i].dest;
-                lSet("current_room", dest);
-                room_look();
-                return;
-            }
-        }
-    }
-
-    //Look for feature commands
-    let features = room.features;
-    console.log(features);
-    for (let i = 0; i < features.length; i++){
-        console.log("feature: ", features[i].desc);
-        for (let j = 0; j < features[i].commands.length; j++){
-            console.log("command: ", features[i].commands[j].desc);
-            for (let k = 0; k < features[i].commands[j].match_phrases.length; k++){
-                let match_phrase = features[i].commands[j].match_phrases[k];
-                console.log("check command phrase:", match_phrase);
-                if (input == match_phrase){
-                    took_action = 1;
-
-                    //Make sure no pre-action checks fail
-                    let pre_action_checks = features[i].commands[j].pre_action_checks;
-                    for (let l = 0; l < pre_action_checks.length; l++){
-                        let check = pre_action_checks[l];
-                        switch(check.type){
-                            case "flag_check":
-                                if (lGet(check.flag) == check.value){
-                                    response_show(check.failed_response);
-                                    return;
-                                }
-                                break;
-                        }
-                    }
-                    //Execute all actions
-                    let actions = features[i].commands[j].actions;
-                    for (let l = 0; l < actions.length; l++){
-                        console.log("action:", actions[l].type);
-                        let action = actions[l];
-                        switch(action.type){
-                            case "response":
-                                response_show(action.text);
-                                break;
-                            case "flag_set":
-                                lSet(action.flag, action.value);
-                                break;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    */
-
     if (took_action == 1){ return; }
     response_show("That command is unknown or meaningless at this time...");
 
@@ -185,31 +120,6 @@ function room_look(){
         }
         //response += " " + contents[i].desc;
     }
-
-
-    /*
-    //Add all exit descriptions to the response
-    let exits = room.exits;
-    for (let i = 0; i < exits.length; i++){
-        response += " " + exits[i].desc;
-    }
-
-    //Add all feature descriptions to the response
-    let features = room.features;
-    for (let i = 0; i < features.length; i++){
-        response += " " + features[i].desc;
-        //Process feature checks
-        let checks = features[i].checks;
-        for (let j = 0; j < checks.length; j++){
-            let flag = checks[j].flag;
-            let value = checks[j].value;
-            let true_response = checks[j].true_response;
-            if (lGet(flag) == value){
-                response += " " + true_response;
-            }
-        }
-    }
-    */
 
     response_show(response);
 }
